b635576addcb1ec1682d7c7b7034e11f2078d555
taon93 <97909995+macnab9393@users.noreply.github.com>
2022-12-14 16:50:06 +0100

    Init commit

---
+++ src/main/java/com/example/pathfindingalgorithmspreviewer/PathFindingAlgorithmsPreviewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathfindingalgorithmspreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathFindingAlgorithmsPreviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathFindingAlgorithmsPreviewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pathfindingalgorithmspreviewer/PathFindingAlgorithmsPreviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathfindingalgorithmspreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathFindingAlgorithmsPreviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
