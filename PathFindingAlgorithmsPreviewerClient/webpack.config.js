4cdb20069a2635e95e59e1b06dd5770557ce1f4b
taon93 <97909995+macnab9393@users.noreply.github.com>
2022-12-14 17:26:53 +0100

    Adds project structure for client part

---
+++ PathFindingAlgorithmsPreviewerClient/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        assetModuleFilename: '[name][ext]'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 5000,
+        hot: true,
+        compress: true,
+        open: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+}
